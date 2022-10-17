698542994138103f4cd2cfb5f9088d8c00f6f31b
vtthinh <vtthinh17st@gmail.com>
2022-10-17 09:29:13 +0700

    Cai dat chuc nang chon san pham yeu thich

--- lib/ui/products/product_grid_tile.dart
+++ lib/ui/products/product_grid_tile.dart
@@ -34,15 +34,20 @@ class ProductGridTile extends StatelessWidget{
   Widget buildGridFooterBar(BuildContext context){
     return GridTileBar(
       backgroundColor: Colors.black87,
-      leading: IconButton(
-        icon: Icon(
-          product.isFavorite? Icons.favorite : Icons.favorite_border
-        ),
-        color: Theme.of(context).colorScheme.secondary,
-        onPressed: () {
-          print('Toggle a favorite product');
+      leading: ValueListenableBuilder<bool>(
+        valueListenable: product.isFavoriteListenable,
+        builder: (context, isFavorite, child) {
+          return IconButton(
+            icon: Icon(
+              isFavorite ? Icons.favorite : Icons.favorite_border
+            ),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {
+              product.isFavorite = !isFavorite;
+            },
+          );
         },
-      ),
+      ), 
       title: Text(
         product.title,
         textAlign: TextAlign.center,
